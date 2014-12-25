e5fc7c5fbb91e4d0e76d6ef9d36ee06e598a2b2b
schahriar <info@schahriar.com>
2014-12-25 04:37:43 -0800

    Version 1.0.0 commit

--- culinary.js
+++ culinary.js
@@ -1,5 +1,10 @@
+"use strict";
 var decode = require('punycode').ucs2.decode;
-module.exports = {
+var style = require('./lib/colors&spices');
+
+var culinary = {
+	style: function(string) { return new style(string, culinary._decode) } ,
+
 	write: function(string) {
 		process.stdout.write(string);
 		return this;
@@ -12,6 +17,18 @@ module.exports = {
 	back: function(integer){ return this.execute(integer + "D") },
 	
 	forth: function(integer){ return this.execute(integer + "C") },
+
+	position: function(x,y) {
+		// This allows dimensions object to be passed without parsing
+		if(x.constructor === Object) {
+			var temp = x;
+			x = temp.columns;
+			y = temp.rows;
+		}
+		return this.execute(y + ";" + x + "H")
+	},
+	
+	dimensions: function() { return { columns: process.stdout.columns, rows: process.stdout.rows, width: process.stdout.columns, height: process.stdout.rows } },
 	
 	scrollUp: function(){ return this.execute("D", false) },
 	scrollDown: function(){ return this.execute("M", false) },
@@ -21,7 +38,9 @@ module.exports = {
 	save: function(){ return this.execute("s") },
 	restore: function(){ return this.execute("u") },
 
-	custom: function(character, prepend, append, hasAttributes){ return this.execute(prepend + character + append, hasAttributes) },
+	_custom: function(character, prepend, append, hasAttributes){ return this.execute(prepend + character + append, hasAttributes) },
+
+	bell: function(){ return this.write("\x07");  },
 
 	clearScreen: function(direction){
 		var directionCodes = {
@@ -53,7 +72,7 @@ module.exports = {
 		this.write(this._decode(prepend+command));
 		return this;
 	},
-	
+
 	_bracket: "[",
 	
 	_decode: function(string){
@@ -61,3 +80,15 @@ module.exports = {
 		return new Buffer([ 0x1b ].concat(decode(string.toString())));
 	}
 }
+
+// Aliases
+culinary.removeLine = culinary.eraseLine;
+culinary.cls = culinary.clearScreen;
+culinary.next = culinary.nextLine;
+culinary.endLine = culinary.nextLine;
+culinary.size = culinary.dimensions;
+culinary.left = culinary.back;
+culinary.right = culinary.forth;
+//
+
+module.exports = culinary;
